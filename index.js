067f44ae42ecc021b6d99c09e121b1cb1c1d34f7
zeMirco <mirco.zeiss@gmail.com>
2014-05-26 19:04:46 +0200

    keep old index file

--- index.js
+++ index.js
@@ -5,32 +5,30 @@ var Session = connect.session.Session;
 var utils = connect.utils;
 
 // export only server method
-module.exports = function(req, res, next) {
+exports.server = function(primus, options) {
+    
+  var store = options.session.store;
+  var key = options.session.key;
+  var secret = options.session.secret;
 
-  // var store = options.session.store;
-  // var key = options.session.key;
-  // var secret = options.session.secret;
-
-  // var cookieSession = !store;
-
-  // if (!secret) throw new Error('`secret` option required for sessions');
-
-  // var Spark = primus.Spark;
-
+  var cookieSession = !store;
+  
+  if (!secret) throw new Error('`secret` option required for sessions');
+  
+  var Spark = primus.Spark;
+  
   // get express session from spark object
-  function sess(req, res, next) {
-
-    console.log(this);
-
+  Spark.prototype.getSession = function(cb) {
+    
     // check headers and query string for cookie
     var rawCookie = this.headers.cookie || this.query.cookie;
-
+    
     // do nothing when there aren't any cookies
     if (!rawCookie) return cb(new Error('no cookie found'));
-
+    
     var signedCookies = cookie.parse(rawCookie);
     var unsignedCookie = utils.parseSignedCookies(signedCookies, secret);
-
+    
     // we have cookie sessions if we don't have a store
     if (cookieSession) {
       // use options or default key
@@ -43,31 +41,23 @@ module.exports = function(req, res, next) {
     // we have a proper session store
     key = key || 'connect.sid';
 
-    req.pause();
-
     // build fake request object
-    // var request = {};
-    // req.cookie = unsignedCookie;
-    // req.sessionID = req.cookie[key];
-    // req.sessionStore = store;
-
+    var req = {};
+    req.cookie = unsignedCookie;
+    req.sessionID = req.cookie[key];
+    req.sessionStore = store;
+    
     // get session from store by passing id
     store.get(req.sessionID, function(err, sess) {
       if (err) return cb(err);
 
-      req.resume();
-
-      // create real session object with all available methods,
+      // create real session object with all available methods, 
       // i.e. destroy, regenerate, etc.
-      // var session = new Session(req, sess);
-      if (session) req.session = session;
-      // cb(null, session);
-      next();
+      var session = new Session(req, sess);
+      cb(null, session);
 
     });
-
-  }
-
-  return sess;
-
+    
+  };
+  
 };
